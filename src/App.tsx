4e76b2c216a774ad72cbc12207e929fe0a6d33e6
Макшанова Алла Андреевна <Makshanova_AA@msk.aep.local>
2024-05-20 11:37:18 +0300

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Workbook } from "@fortune-sheet/react";
+import "@fortune-sheet/react/dist/index.css"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Workbook data={[{ name: "Sheet1" }]} />
 }
 
 export default App;
